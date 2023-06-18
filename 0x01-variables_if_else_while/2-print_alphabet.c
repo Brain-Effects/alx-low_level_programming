e79bd74dba93ea3577ceb2a2936b0bfce4817e21
--add <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@github.com>
2023-06-18 15:57:51 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	
+       for (c = 'a'; c <= 'z'; c++)
+       {
+		putchar(c);
+		putchar('\n');
+       }
+
+		return (0);
+}
+
