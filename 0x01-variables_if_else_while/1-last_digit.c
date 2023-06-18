1f5acd36af7316fcc5ae02745157b6c782482ca4
--add <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@github.com>
2023-06-18 15:34:14 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,12 +3,6 @@
 #include <stdio.h>
 
 /**
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
- */
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
