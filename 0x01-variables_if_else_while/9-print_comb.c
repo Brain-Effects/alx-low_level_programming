83abeb3ef818fa99225d896c0d9f597c6f4b5de4
--add <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@github.com>
2023-06-18 20:08:19 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,19 +11,14 @@ int main(void)
 	for (i = 123; i <= 789; i++)
 	{
 	if (i % 10 > i / 10 % 10 && i / 10 % 10 > i / 100)
-	{
+	}
 		putchar(i / 100 + '0');
 		putchar(i / 10 % 10 + '0');
 		putchar(i % 10 + '0');
 	if (i < 789)
 	{
-		putchar(',');
-		putchar(' ');
-		}
+		putchar(',') (' ');
 	}
-	}
-		putchar('\n');
 
 	return (0);
 }
-
