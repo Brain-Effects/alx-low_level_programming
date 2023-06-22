adf3b5e56b1bcca38593fe5fda6da0f3f5a81cf3
--add <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@github.com>
2023-06-21 17:58:24 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,14 @@
  *
  * Return: the value of the last digit
  */
-
-int print_last_digit(int)
+int print_last_digit(int n)
 {
-	last_digit = print_last_digit(10);
-		_putchar(last_digit + '0');
-		_putchar('\n');
+	int last_digit = n % 10;
+	{
+	if (last_digit < 0)
+	last_digit = -last_digit;
+	}
+	_putchar(last_digit + '0');
+	_putchar('\n');
 	return (last_digit);
 }
