be5aec7f9298fd04f4ce12f74ea2f52a84443312
--add <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@github.com>
2023-06-22 12:07:33 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "unistd.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative.
 * Return: Always 0
 */
 
