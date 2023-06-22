02ef4402fb446790e1ede0397fb40694729d9f4d
--add <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@github.com>
2023-06-22 08:54:56 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 	int i;
 
 	for (i = 123; i <= 789; i++)
-	{
+
 	if (i % 10 > i / 10 % 10 && i / 10 % 10 > i / 100)
-	}
+
 		putchar(i / 100 + '0');
 		putchar(i / 10 % 10 + '0');
 		putchar(i % 10 + '0');
 	if (i < 789)
 	{
-		putchar(',') (' ');
+		putchar('\n');
 	}
 
 	return (0);
