2fb5633cc8001bfac9c5a83ddef0ff6f797c86d0
--add <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@github.com>
2023-06-22 10:03:29 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * @i: the integer that need to be be printed
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 
 int main(void)
