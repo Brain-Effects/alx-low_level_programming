1e306df803114fa1aebaf71214ac33df60e270ec
--add <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@github.com>
2023-06-22 19:51:12 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,54 @@
+#include "stdlib.h"
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		_putchar('F');
+		_putchar('i');
+		_putchar('z');
+		_putchar('z');
+		_putchar('B');
+		_putchar('u');
+		_putchar('z');
+		_putchar('z');
+	}
+	else if (i % 3 == 0)
+	{
+		_putchar('F');
+		_putchar('i');
+		_putchar('z');
+		_putchar('z');
+	}
+	else if (i % 5 == 0)
+	{
+		_putchar('B');
+		_putchar('u');
+		_putchar('z');
+		_putchar('z');
+	}
+	else
+		{
+		print_number(i);
+		}	
+
+	if (i < 100)
+	{
+	_putchar(' ');
+	}
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
