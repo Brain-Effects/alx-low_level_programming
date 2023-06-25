91123cc3c693b8a5d4fff9c93c49e3e861650f4b
--add <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@github.com>
2023-06-25 16:01:00 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,8 @@ void print_to_98(int n)
 	{
 	for (i = n; i < 98; i++)
 	{
+		_putchar(i % 10 + '0');
+		_putchar(i / 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
@@ -22,6 +24,8 @@ void print_to_98(int n)
 	{
 	for (i = n; i > 98; i--)
 	}
+		_putchar(i % 10 + '0');
+		_putchar(i / 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
