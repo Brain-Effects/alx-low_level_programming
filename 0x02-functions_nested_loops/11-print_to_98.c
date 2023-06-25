e8e6786ab6c7dc8f992ded89deef614c51072a23
--add <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@github.com>
2023-06-25 15:30:56 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,37 @@
-#include "unistd.h"
 #include "main.h"
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
+ * @n: starting number
  *
- * Description: Numbers are separated by a comma and a space, and are printed
- * in order. The first printed number is n and the last printed number is 98.
+ * Return: void
  */
-
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-	for (; n < 98; n++)
+	for (i = n; i < 98; i++)
+	{
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
 	}
 	else
 	{
-	for (; n > 98; n--)
+	for (i = n; i > 98; i--)
 	{
-	print_number(n);
-	_putchar(',');
-	_putchar(' ');
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
 	}
 	}
-	print_number(n);
+
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
-}
-	void print_number(int n)
-{
-	int i, j, div;
-
-	if (n < 0)
-	{
-	_putchar('-');
-	n = -n;
-	}
-
-	div = 1;
-	for (i = n; i >= 10; i /= 10)
-	div *= 10;
-
-	for (j = div; j >= 1; j /= 10)
-	_putchar(n / j % 10 + '0');
 }
