5c46b5cc61b668e8604167248885e09a84abce2f
--add <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@github.com>
2023-06-26 16:59:28 -0700

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the first character of the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+	length++;
+	for (int i = 0; i < length / 2; i++)
+	{
+	char temp = s[i];
+	s[i] = s[length - i - 1];
+	s[length - i - 1] = temp;
+	}
+}
