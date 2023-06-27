19113933e9493a91c7f2e1a529d5f7b23c11a66d
--add <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@github.com>
2023-06-26 17:42:18 -0700

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, n, length = 0;
+
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+
+	if (length % 2 == 0)
+	{
+	n = length / 2;
+	}
+	else
+	{
+	n = (length - 1) / 2;
+	}
+
+	for (i = n; i < length; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
