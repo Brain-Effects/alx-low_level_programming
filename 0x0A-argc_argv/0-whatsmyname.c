6b2e6d6377698a2d85e9e62b7f844ac166aa03c4
--add <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@github.com>
2023-07-11 01:00:32 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+  * main - prints out the number of things put on the command line
+  * @argc: The integer value to be printed
+  * @argv: The string that needs be printed
+  *
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
