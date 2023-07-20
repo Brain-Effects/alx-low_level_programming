387728c5168b50de1996d1ad58925ca07ff1f8c3
--add <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@github.com>
2023-07-19 19:33:02 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to print
+ * @f: the function to use to print the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+/**
+ * print_upper - prints a name in uppercase
+ * @name: the name to print
+ *
+ * Return: void
+ */
+void print_upper(char *name)
+{
+	int i = 0; i
+
+	while (name[i])
+	{
+		putchar(toupper(name[i]));
+		i++;
+	}
+	putchar('\n');
+}
