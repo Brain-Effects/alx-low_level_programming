bb95fa7b33561e092bb0b9364d8c6a95a3754cb9
--add <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@github.com>
2023-07-20 01:53:22 -0700

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - a new type that defines a dog
+ * struct dog - a dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
