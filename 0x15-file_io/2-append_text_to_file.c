dda0831cb661b8589a05f395fb0e517a154ca7bb
--add <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@github.com>
2023-08-07 15:27:45 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
  * Do not create the file if it does not exist
  * If filename is NULL return -1
  * If text_content is NULL, do not add anything to the file.
- * Return 1 if the file exists and -1 if the file does not exist or if 
+ * Return 1 if the file exists and -1 if the file does not exist or if
  * you do not have the required permissions to write the file.
  */
 int append_text_to_file(const char *filename, char *text_content)
