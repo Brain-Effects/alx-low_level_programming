eb9c81b4f9427c77d1a3fec194124858316d490b
--add <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@github.com>
2023-08-07 14:55:33 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,6 +5,16 @@
 #include <sys/stat.h>
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to be read and printed
+ *
+ * Return: actual number of letters it could read and print
+ * If the file can not be opened or read, return 0
+ * If filename is NULL return 0
+ * If write fails or does not write the expected amount of bytes, return 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
