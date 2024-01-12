570e47ed379536da5500d49e6ea71c1006f12d7f
Brain-Effects <braineffects@github.com>
2024-01-12 01:38:14 -0800

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,15 +9,18 @@
  */
 dlistint_t *create_new_node(int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL) return NULL;
-    new_node->n = n;
-    return new_node;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	return (new_node);
 }
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: Double pointer to the head of the list.
- * @idx: The index of the list where the new node should be added. Index starts at 0.
+ * @idx: The index of the list where the new node should be added.
+ * Index starts at 0.
  * @n: The integer to be included in the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
@@ -27,26 +30,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL || (idx != 0 && *h == NULL)) return NULL;
+	if (h == NULL || (idx != 0 && *h == NULL))
+		return (NULL);
 	new_node = create_new_node(n);
-	if (new_node == NULL) return NULL;
+	if (new_node == NULL)
+		return (NULL);
 
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
-		if (*h != NULL) (*h)->prev = new_node;
+		if (*h != NULL)
+		(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 
-	while (temp && ++i < idx) temp = temp->next;
-	if (temp == NULL) { free(new_node); return NULL; }
+	while (temp && ++i < idx)
+		temp = temp->next;
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->next = temp->next;
 	new_node->prev = temp;
-	if (temp->next != NULL) temp->next->prev = new_node;
-	temp->next = new_node;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+		temp->next = new_node;
 
 	return (new_node);
 }
