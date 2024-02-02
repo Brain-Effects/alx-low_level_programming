cd2b0bb368b5db52a4a5bf145623b4031723f895
Brain-Effects <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@github.com>
2024-02-02 14:52:46 -0800

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * If ht is NULL, don’t print anything
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char *sep = "";
+
+	if (ht == NULL)
+	return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+	node = ht->array[i];
+	while (node != NULL)
+		{
+		printf("%s'%s': '%s'", sep, node->key, node->value);
+		sep = ", ";
+		node = node->next;
+		}
+	}
+	printf("}\n");
+}
